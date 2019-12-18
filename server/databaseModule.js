9ee94b1df6de2424a6de8ea30826c0aef04c7934
Corentin AMIEL <corentin.amiel@etu.univ-grenoble-alpes.fr>
2019-12-18 10:42:02 +0100

    verification des données d'entrée pour le type Case + nettoyage du code

--- server/databaseModule.js
+++ server/databaseModule.js
@@ -6,9 +6,7 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  //console.log("connecté!");
-});
+db.once('open', function() {});
 
 /**
  * @typedef Player
@@ -30,11 +28,6 @@ var playerSchema = new mongoose.Schema({
 
 var Player = mongoose.model('Player', playerSchema);
 
-//var Corentin = new Player({ id: 1, pseudo: "Corentin", rating:2000 });
-//var Victor = new Player({ id: 2, pseudo: "Victor", rating:2001 });
-//var Orlane = new Player({ id: 3, pseudo: "Orlane", rating:2200 });
-//var Quentin = new Player({ id: 4, pseudo: "Quentin", rating:1999 });
-
 /**
  * @typedef Square
  * @type {object}
@@ -45,25 +38,21 @@ var Player = mongoose.model('Player', playerSchema);
 /** @type {Square} */
 
 var squareSchema = new mongoose.Schema({
-  id: {type: String,  enum:['A1','A2','A3','A4']}, //TODO extends values
+  id: {type: String,  enum:[
+  'A1','A2','A3','A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 
+  'B1','B2','B3','B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10',
+  'C1','C2','C3','C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10',
+  'D1','D2','D3','D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10',
+  'E1','E2','E3','E4', 'E5', 'E6', 'E7', 'E8', 'E9', 'E10',
+  'F1','F2','F3','F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10',
+  'G1','G2','G3','G4', 'G5', 'G6', 'G7', 'G8', 'G9', 'G10',
+  'H1','H2','H3','H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10',
+  'I1','I2','I3','I4', 'I5', 'I6', 'I7', 'I8', 'I9', 'I10',
+  'J1','J2','J3','J4', 'J5', 'J6', 'J7', 'J8', 'J9', 'J10']}, 
   content: {type: Number, min:0, max: 4}
 });
 
 var Square = mongoose.model('Square', squareSchema);
-
-var A1 = new Square({ id: 'A1', content:0 });
-var A2 = new Square({ id: 'A2', content:0 });
-var A3 = new Square({ id: 'A3', content:0 });
-var A4 = new Square({ id: 'A4', content:0 });
-
-
-/*var boardSchema = new mongoose.Schema({
-    board: [squareSchema],
-    player: playerSchema,
-    date: {type:Date, default: Date.now}
-});
-
-var Board = mongoose.model('Board', boardSchema); */
 
 /**
  * @typedef Move
@@ -99,99 +88,6 @@ var gameSchema = new mongoose.Schema({
 
 var Game = mongoose.model('Game', gameSchema);
 
-/*var game1 = new Board({
-    board: [{}],
-    player: Corentin,
-});*/
-
-/*function addPlayers(){
-Corentin.save(function (err) {
-  if (err) { throw err; }
-  else {
-    console.log(' Corentin ajouté avec succès !');
-    Victor.save(function (err) {
-      if (err) { throw err; }
-      else{
-        console.log(' Victor ajouté avec succès !');
-        Orlane.save(function (err) {
-          if (err) { throw err; }
-          else{
-            console.log(' Orlane ajouté avec succès !');
-            Quentin.save(function (err) {
-              if (err) { throw err; }
-              else{
-                console.log(' Quentin ajouté avec succès !');
-                findPlayer();
-                addSquare();
-              }
-            });
-          }
-        });
-      }
-    });
-  }
-}); 
-} */
-
-/*function findPlayer(){
-  Player.find({pseudo: "Orlane"}, function (err, comms) {
-    if (err) { throw err; }
-    else{
-        // comms est un tableau de hash
-        console.log("resultat du find : "+comms);
-    }
-  });
-}*/
-
-/* function addSquare(){
-A1.save(function (err) {
-  if (err) { throw err; }
-  else {
-    console.log(' A1 ajouté avec succès !');
-    A2.save(function (err) {
-      if (err) { throw err; }
-      else{
-        console.log(' A2 ajouté avec succès !');
-        A3.save(function (err) {
-          if (err) { throw err; }
-          else{
-            console.log(' A3 ajouté avec succès !');
-            A4.save(function (err) {
-              if (err) { throw err; }
-              else{
-                console.log(' A4 ajouté avec succès !');
-                addBoard();
-              }
-            });
-          }
-        });
-      }
-    });
-  }
-});   
-          
-}*/
-
-/** function addBoard(){
-  game1.save(function (err) {
-    if (err) { throw err; }
-    else{
-      console.log(' game1 ajouté avec succès !');
-      findBoard();
-    }
-  });
-}
-
-function findBoard(){
-Board.find({}, function (err, comms) {
-  if (err) { throw err; }
-  else{
-      // comms est un tableau de hash
-      console.log("resultat du find : "+comms);
-  }
-});
-} */
-
 /**
  * add a player to the database
  * @param {number} Pid - an id
@@ -199,12 +95,12 @@ Board.find({}, function (err, comms) {
  * @param {number} Prating - a rating
  * @param {string} Ppassword - a password
  */
+
 function addAPlayer(Pid, Ppseudo, Prating){
     var newPlayer = new Player({ id: Pid, pseudo: Ppseudo, rating:Prating, password:Ppassword });
     newPlayer.save(function (err) {
         if (err) { throw err; }
         else{
-          //console.log(newPlayer.pseudo+" ajouté avec succès !");
         }
     });
 }
@@ -227,7 +123,7 @@ function addAPlayer(Pid, Ppseudo, Prating){
      newGame.save(function (err) {
         if (err) { throw err; }
         else{
-          //console.log(newGame.playerWhite.pseudo +" vs "+newGame.playerBlack.pseudo+ " ajouté avec succès");
+          
         }
     });
  }
