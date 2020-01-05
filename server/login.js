1753f095360decc70037e3e22e2201011880c28d
orlanepitarch <orlane.pitatch@gmail.com>
2020-01-05 18:20:57 +0100

    little forgotten fix

--- server/login.js
+++ server/login.js
@@ -3,7 +3,7 @@ let db = require('./databaseModule');
 async function connexion(pseudo, mdp) {
     const userPromise = await db.findAPlayerByPseudo(pseudo, mdp);
     if (userPromise == "user unknown") {
-        db.addAPlayer(pseudo, 0, mdp);
+        db.addAPlayer(pseudo, mdp);
     }
     else if (userPromise == "Mauvais mot de passe") {
         return "Mauvais mot de passe";
